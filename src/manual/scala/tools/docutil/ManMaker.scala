ffe469919d28327626bbe39065ab3d0f02ba7c15
Som Snytt <som.snytt@gmail.com>
2017-12-29 16:17:58 -0800

    ManMaker is not ant task

--- src/manual/scala/tools/docutil/ManMaker.scala
+++ src/manual/scala/tools/docutil/ManMaker.scala
@@ -6,64 +6,31 @@
 
 package scala.tools.docutil
 
-import org.apache.tools.ant.Task
-
 import java.io.{File, FileOutputStream}
 
-class ManMaker extends Task {
+class ManMaker(htmlout: File, manout: File) {
 
-  /** The command for which to generate the man page */
-  private var command: List[String] = Nil
+  def makeMan(command: String): Unit = {
+    val classname = "scala.man1."+ command
 
-  /** The directory to put html pages in */
-  private var htmlout: Option[File] = None
+    val htmlFileName = htmlout.getPath + File.separator +
+                       command + ".html"
+    val htmlFile = new java.io.FileOutputStream(htmlFileName)
+    EmitHtml.emitHtml(classname, htmlFile)
 
-  /** The directory to put man pages in */
-  private var manout: Option[File] = None
-
-
-  def setCommand(input: String) {
-    command = input.split(",").toList.flatMap { s =>
-      val st = s.trim()
-      if (st != "") List(st) else Nil
-    }
-  }
-
-  def setHtmlout(input: File) {
-    htmlout = Some(input)
-  }
-
-  def setManout(input: File) {
-    manout = Some(input)
-  }
-
-  override def execute() {
-    if (command.isEmpty) sys.error("Attribute 'command' is not set.")
-    if (htmlout.isEmpty) sys.error("Attribute 'htmlout' is not set.")
-    if (manout.isEmpty) sys.error("Attribute 'manout' is not set.")
-
-    command foreach (cmd => {
-      val classname = "scala.man1."+ cmd
-
-      val htmlFileName = htmlout.get.getPath + File.separator +
-                         cmd + ".html"
-      val htmlFile = new java.io.FileOutputStream(htmlFileName)
-      EmitHtml.emitHtml(classname, htmlFile)
-
-      val manFileName = manout.get.getPath + File.separator +
-                        "man1" + File.separator + cmd + ".1"
-      val manFile = new FileOutputStream(manFileName)
-      EmitManPage.emitManPage(classname, manFile)
-    })
+    val manFileName = manout.getPath + File.separator +
+                      "man1" + File.separator + command + ".1"
+    val manFile = new FileOutputStream(manFileName)
+    EmitManPage.emitManPage(classname, manFile)
   }
 }
 
 /** Command line runner for ManMaker which is called from the sbt build. */
-object ManMaker extends App {
-  val Array(commands, htmlout, manout) = args
-  val mm = new ManMaker
-  mm.setCommand(commands)
-  mm.setHtmlout(new File(htmlout))
-  mm.setManout(new File(manout))
-  mm.execute()
+object ManMaker {
+  def main(args: Array[String]): Unit = {
+    val Array(commands, htmlout, manout) = args
+    val mm = new ManMaker(new File(htmlout), new File(manout))
+    for (command <- commands.split(",").map(_.trim()) if command.nonEmpty)
+      mm.makeMan(command)
+  }
 }
