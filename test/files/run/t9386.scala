1a7a95386807d138f5fe57dc26edd0b26a82ce20
Som Snytt <som.snytt@gmail.com>
2019-12-10 11:46:00 -0800

    fix

--- test/files/run/t9386.scala
+++ test/files/run/t9386.scala
@@ -1,6 +1,6 @@
 import scala.language.implicitConversions
 class Foo
-object Foo { 
+object Foo {
   implicit def int2Foo(a: => Int): Foo = new Foo //Never evaluate the argument
   def bar(foo: Foo) = ()
   def bar(foo: Boolean) = () //unrelated overload
